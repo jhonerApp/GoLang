5f2081509e1b490a82b39a5d338e6f9b77e73375
jhonerApp <jhonerApp@gmail.com>
2024-07-03 19:56:15 +0800

    Go lang tutorial

---
+++ booking-app/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 30
+	bookings := []string{} //or var bookings []string or var bookings = []string{} -> syntax to assign value in array variable
+
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n ", conferenceTickets, remainingTickets, conferenceName)
+	fmt.Printf("We have total of %v, conferenceTickets, tickets and %v, are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	//& = pointers or special variable / convert 0x000134
+	fmt.Println(("Enter your first name: "))
+	fmt.Scan(&firstName)
+
+	fmt.Println(("Enter your last name: "))
+	fmt.Scan(&lastName)
+
+	fmt.Println(("Enter your email: "))
+	fmt.Scan(&email)
+
+	fmt.Println(("Enter number of  tickets: "))
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Slice type: %T\n", bookings)
+	fmt.Printf("Slice length: %v\n", len(bookings))
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for  %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all our bookings: %v\n", bookings)
+}
